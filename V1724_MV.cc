0ebf6ca5153a3afa101bc109549fd0542c80bf86
Daniel Coderre <coderdj@gmail.com>
2019-10-16 12:36:31 +0000

    'git add' is also a thing

---
+++ V1724_MV.cc
@@ -0,0 +1,8 @@
+#include "V1724_MV.hh"
+
+V1724_MV::V1724_MV(MongoLog *log, Options *options)
+  :V1724(log, options){
+  DataFormatDefinition["channel_header_size"] = 0;
+}
+
+V1724_MV::~V1724_MV(){};
---
+++ V1724_MV.hh
@@ -0,0 +1,16 @@
+#ifndef _V1724_MV_HH_
+#define _V1724_MV_HH_
+
+#include "V1724.hh"
+
+class V1724_MV : public V1724 {
+
+public:
+  V1724_MV(MongoLog *log, Options *options);
+  virtual ~V1724_MV();
+
+private:
+
+};
+
+#endif
