38923873396dc0966df244fb14105d6168e578b5
xenon <xenon@xenondaq.na.infn.it>
2019-12-18 16:12:32 +0100

    Test with 1730

---
+++ V1730.hh
@@ -0,0 +1,16 @@
+#ifndef _V1730_HH_
+#define _V1730_HH_
+
+#include "V1724.hh"
+
+class V1730 : public V1724 {
+
+public:
+  V1730(MongoLog *log, Options *options);
+  virtual ~V1730();
+
+private:
+
+};
+
+#endif
