d56be2b9caaef533618ab02aab828759580ad3d2
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2020-03-17 16:12:30 +0100

    Some these slipped through

--- Options.hh
+++ Options.hh
@@ -86,7 +86,7 @@ public:
   std::vector<u_int16_t> GetThresholds(int board);
 
   void UpdateDAC(std::map<int, std::map<std::string, std::vector<double>>>&);
-  void SaveBenchmarks(std:map<std::string, long>&, std::map<int, long>& double, double);
+  void SaveBenchmarks(std::map<std::string, long>&, std::map<int, long>&, double, double);
 private:
   mongocxx::client fClient;
   bsoncxx::document::view bson_options;
